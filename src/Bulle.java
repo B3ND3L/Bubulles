c8d7e54e1cb34080238570f74563fb9c443952b4
LaurentChion <laurent.chion@gmail.com>
2016-09-29 17:54:28 +0200

    Changement des coordonnées int => double Ajout Get et Set des coordonées

--- src/Bulle.java
+++ src/Bulle.java
@@ -1,6 +1,27 @@
 
 public class Bulle {
-    private int x;
-    private int y;
-    private int z;
+
+    private double x;
+    private double y;
+    private double z;
+
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getZ(){
+        return z;
+    }
+
+    public double setX(double x){
+        this.x = x;
+    }
+    public double setY(double y){
+        this.y = y;
+    }
+    public double setZ(double z){
+        this.z = z;
+    }
 }
