40847925b71c7fcdbdebcd0b8ab86c058aed735c
LaurentChion <laurent.chion@gmail.com>
2016-10-03 16:38:27 +0200

    Ajout de "l'autre" viewer (ui)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		/* Meilleur Viewer */
-		//System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
 		Parser p = new Parser();
 		Graph g = p.parse("norma_N5_tau4_dt2_delai820_000003.txt");
