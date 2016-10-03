f30aa3f3038886b4da0fd52fb354f22a8caa3997
LaurentChion <laurent.chion@gmail.com>
2016-10-03 15:21:31 +0200

    Ajout d'une position fixe pour chaque bulle dans le graphe.

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/* Meilleur Viewer */
+		//System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+
 		Parser p = new Parser();
 		Graph g = p.parse("norma_N5_tau4_dt2_delai820_000003.txt");
 		g.addAttribute("ui.antialias");
 		g.addAttribute("ui.quality");
-		
+
 		System.out.println(g.getNodeCount());
 		System.out.println(g.getEdgeCount());
-		
-		g.display(true);
+
+		g.display(false);
 	}
 
 }
--- src/Parser.java
+++ src/Parser.java
@@ -9,64 +9,69 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 
 public class Parser {
-	
+
 	public Graph parse(String filename){
-		
+
 		String path = "";
 		File file = new File(path + filename);
-		
+
 		ArrayList<Bulle> a = readFile(file);
 		return listToGraph(a);
 	}
-	
+
 	protected ArrayList<Bulle> readFile(File file){
-		
+
 		ArrayList<Bulle> bulles = new ArrayList<Bulle>();
-		
+
 		try{
-			InputStream ips=new FileInputStream(file); 
+			InputStream ips=new FileInputStream(file);
 			InputStreamReader ipsr=new InputStreamReader(ips);
 			BufferedReader br=new BufferedReader(ipsr);
 			String line;
 			while ((line=br.readLine())!=null){
-								
+
 				double x, y, z;
-				
+
 				String [] coords = line.split("   ");
-				
+
 				x = Double.parseDouble(coords[1]);
 				y = Double.parseDouble(coords[2]);
 				z = Double.parseDouble(coords[3]);
-					
-				
+
+
 				Bulle bulle = new Bulle(x,y,z);
 				bulles.add(bulle);
 			}
-			br.close(); 
-		}		
+			br.close();
+		}
 		catch (Exception e){
 			System.out.println(e.toString());
 		}
-		
+
 		return bulles;
-	}	
-	
+	}
+
 	protected static boolean hasBulle(Bulle[] tab, Bulle bulle){
-		
+
 		for(Bulle b:tab) if(b==bulle) return true;
 		return false;
 	}
-	
+
 	protected Graph listToGraph(ArrayList<Bulle> list){
 		Graph graph = new SingleGraph("graph");
-		
+
 		for(Bulle b1 : list){
 			graph.addNode(b1.getId()+"");
+
+			// Ajout de la position de la bulle dans le graphe.
+			graph.getNode(b1.getId()+"").setAttribute("x",b1.getX());
+			graph.getNode(b1.getId()+"").setAttribute("y",b1.getY());
+			graph.getNode(b1.getId()+"").setAttribute("z",b1.getZ());
 		}
-		
-		for(Bulle b1 : list){	
+
+		for(Bulle b1 : list){
 			Bulle [] tab = new Bulle[4];
-			
+
 			for(Bulle b2 : list){
 				double dist = b1.computeDistance(b2);
 				for(int i=0;i<4;i++){
@@ -78,16 +83,16 @@ public class Parser {
 					}
 				}
 			}
-			
+
 			for(int i=0; i<4; i++){
 				if(graph.getEdge(b1.getId()+"-"+tab[i].getId()) == null)
 					try{
 						graph.addEdge(b1.getId()+"-"+tab[i].getId(), b1.getId()+"", tab[i].getId()+"");
 					}catch(Exception e){
-						
+
 					}
 			}
-			
+
 		}
 		return graph;
 	}
